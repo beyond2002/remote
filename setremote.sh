4bf305373bc1ed93d9443f2538c31fc3cecf46b1
beyond2002 <beyond2002@gmail.com>
2019-12-29 12:31:21 +1000

    Create setremote.sh

---
+++ setremote.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt update
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
+echo "xfce4-session" > ~/.chrome-remote-desktop-session
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
