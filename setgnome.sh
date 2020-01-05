a2a5eb887b0717facf78db41ac5820cf6d6f2cc5
beyond2002 <beyond2002@gmail.com>
2020-01-05 22:51:20 +1000

    Create setgnome.sh

---
+++ setgnome.sh
@@ -0,0 +1,8 @@
+$ sudo apt-get update
+$ sudo apt-get upgrade
+$ sudo apt-get install gnome-shell
+$ sudo apt-get install ubuntu-gnome-desktop
+$ sudo apt-get install autocutsel
+$ sudo apt-get install gnome-core
+$ sudo apt-get install gnome-panel
+$ sudo apt-get install gnome-themes-standard
