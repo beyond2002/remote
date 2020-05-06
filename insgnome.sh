a6ca9fa04a574a88210b167e96f82b3935215339
Dong <beyond2002@gmail.com>
2020-05-06 07:15:59 -0400

    add install gnome script

---
+++ insgnome.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo xargs -a packages_list.txt apt install
+sudo apt-get install gnome-shell ubuntu-desktop ubuntu-gnome-desktop autocutsel gnome-core gnome-panel gnome-themes-standard xserver-xorg xserver-xorg-core xserver-xorg-video-dummy anydesk -y
+sudo cp xorg.conf /etc/X11/xorg.conf
