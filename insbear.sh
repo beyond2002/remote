d3828d4449cfcc69d7e3e25ed237db9be8489a24
Dong <beyond2002@gmail.com>
2020-05-06 07:42:13 +0000

    Modify install bear script

--- insbear.sh
+++ insbear.sh
@@ -9,14 +9,14 @@ mkdir build32
 cd ~/build32
 cmake "$BEAR_SOURCE_DIR" -DCMAKE_C_COMPILER_ARG1="-m32"
 VERBOSE=1 make all
+sudo make install
 cd ~
 mkdir build64
 cd ~/build64
-cmake "$BEAR_SOURCE_DIR" -DCMAKE_C_COMPILER_ARG1="-m64"
+cmake "$BEAR_SOURCE_DIR" -DCMAKE_C_COMPILER_ARG1="-m64" -DDEFAULT_PRELOAD_FILE='/opt/bear/$LIB/libear.so'
 VERBOSE=1 make all
-sudo make install
 cd ~
 sudo install -m 0644 ~/build32/libear/libear.so  /lib/i386-linux-gnu
-sudo install -m 0644 ~/build32/libear/libear.so  /lib/x86_64-linux-gnu
+sudo install -m 0644 ~/build64/libear/libear.so  /lib/x86_64-linux-gnu
 
 
