15bd53634f6edb5b3dc3e34f076ffe661c749a31
Dong <beyond2002@gmail.com>
2020-05-10 19:15:41 -0400

    update insgome.sh

--- insgnome.sh
+++ insgnome.sh
@@ -6,3 +6,5 @@ sudo apt-get upgrade -y
 sudo xargs -a packages_list.txt apt install
 sudo apt-get install gnome-shell ubuntu-desktop ubuntu-gnome-desktop autocutsel gnome-core gnome-panel gnome-themes-standard xserver-xorg xserver-xorg-core xserver-xorg-video-dummy anydesk -y
 sudo cp xorg.conf /etc/X11/xorg.conf
+sudo su
+echo gnomevm@1 | anydesk --set-password
