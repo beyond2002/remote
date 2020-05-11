db21b680e1cd1ff87c68a435c9a84b72738cf6a9
Dong <beyond2002@gmail.com>
2020-05-10 23:17:25 -0400

    update scripts

---
+++ inscrome.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
--- insgnome.sh
+++ insgnome.sh
@@ -1,10 +1,12 @@
 #/bin/sh
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+#wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+#echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo xargs -a packages_list.txt apt install
-sudo apt-get install gnome-shell ubuntu-desktop ubuntu-gnome-desktop autocutsel gnome-core gnome-panel gnome-themes-standard xserver-xorg xserver-xorg-core xserver-xorg-video-dummy anydesk -y
+apt-get install ubuntu-desktop -y
+apt-get install xserver-xorg-video-dummy -y
+#sudo xargs -a packages_list.txt apt install
+#sudo apt-get install gnome-shell ubuntu-desktop ubuntu-gnome-desktop autocutsel gnome-core gnome-panel gnome-themes-standard xserver-xorg xserver-xorg-core xserver-xorg-video-dummy anydesk -y
 sudo cp xorg.conf /etc/X11/xorg.conf
-sudo su
-echo gnomevm@1 | anydesk --set-password
+#sudo su
+#echo gnomevm@1 | anydesk --set-password
